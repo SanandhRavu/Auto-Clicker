d07bed9e8dc901adb79be27d3c20c4ca2f9f59cb
Sanandh Ravu <sanandhr@gmail.com>
2020-12-22 19:29:27 -0500

    Update autoclickui.py
    updated to newer version
    
    WIP:
    - no changes

--- autoclickui.py
+++ autoclickui.py
@@ -1,180 +1,146 @@
-import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QToolTip, QPushButton,
-                             QMessageBox, QHBoxLayout, QVBoxLayout,
-                             QTextEdit, QGridLayout, QLineEdit, QLabel, QSizePolicy, QSpinBox)
-from PyQt5 import QtCore, QtGui
-import pyautogui
+import sys, pyautogui
+from PyQt5 import QtWidgets, QtCore, QtGui
 from functools import partial
 
-
-class Window(QWidget):
+class mainWindow(QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
 
-        
-
         self.initUI()
 
-
     def initUI(self):
-        QToolTip.setFont(QtGui.QFont('SansSerif', 10))
-
-
-        # ---------- widgets ------------------------------
         
-        self.xEdit = QSpinBox()
-        self.xEdit.setMaximum(10000)
-        self.yEdit = QSpinBox()
-        self.yEdit.setMaximum(10000)
+        # ------------ spinboxes ------------ #
+        self.x = spinBox()
+        self.y = spinBox()
+        self.duration = spinBox()
+        self.frequency = spinBox()
 
-        self.duration = QLabel('Duration')
-        self.frequency = QLabel('Frequency')
-        self.xCoordinate = QLabel('X Coordinate')
-        self.yCoordinate = QLabel('Y Coordinate')
+        # ------------ labels ------------ #
+        self.xLabel = QtWidgets.QLabel('X Coordinate')
+        self.yLabel = QtWidgets.QLabel('Y Coordinate')
+        self.durLabel = QtWidgets.QLabel('Duration')
+        self.freqLabel = QtWidgets.QLabel('Frequency')
 
-        self.durationEdit = QSpinBox()
-        self.frequencyEdit = QSpinBox()
+        # ------------ buttons ------------ #
+        self.coordBtn = QtWidgets.QPushButton('Get Coordinates')
+        self.coordBtn.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.okBtn = QtWidgets.QPushButton('OK')
+        self.cancelBtn = QtWidgets.QPushButton('Cancel')
 
-        self.coordButton = QPushButton('Get Coordinates', self)
-        self.okButton = QPushButton('OK', self)
-        self.cancelButton = QPushButton('Cancel')
+        # ------------ tooltips ------------ #
+        self.xLabel.setToolTip('Set x position of mouse')
+        self.yLabel.setToolTip('Set y position of mouse')
+        self.durLabel.setToolTip('Set how many minutes to activate the clicker')
+        self.freqLabel.setToolTip('Set how many times per minute to click')
 
-
-        # ---------- tooltips ------------------------------
-        
-        self.duration.setToolTip('Set how many minutes to activate the clicker')
-        self.duration.resize(self.duration.sizeHint())
-
-        self.frequency.setToolTip('Set how many times per minute to click')
-        self.frequency.resize(self.frequency.sizeHint())
-
-        self.xCoordinate.setToolTip('Set x position of mouse')
-        self.xCoordinate.resize(self.xCoordinate.sizeHint())
-
-        self.yCoordinate.setToolTip('Set y position of mouse')
-        self.yCoordinate.resize(self.yCoordinate.sizeHint())
-
-        # ---------- layout ------------------------------
-        #
-        grid = QGridLayout()
+        # ------------ grid ------------ #
+        grid = QtWidgets.QGridLayout()
         grid.setSpacing(10)
 
-        grid.addWidget(self.duration, 3, 0)
-        grid.addWidget(self.durationEdit, 3, 1)
-        
+        grid.addWidget(self.xLabel, 1, 0)
+        grid.addWidget(self.x, 1, 1)
 
-        grid.addWidget(self.frequency, 4, 0)
-        grid.addWidget(self.frequencyEdit, 4, 1)
+        grid.addWidget(self.yLabel, 2, 0)
+        grid.addWidget(self.y, 2, 1)
 
-        grid.addWidget(self.xCoordinate, 1, 0)
-        grid.addWidget(self.xEdit, 1, 1)
+        grid.addWidget(self.coordBtn, 1, 2, 2, 1)
 
-        grid.addWidget(self.yCoordinate, 2, 0)
-        grid.addWidget(self.yEdit, 2, 1)
+        grid.addWidget(self.durLabel, 3, 0)
+        grid.addWidget(self.duration, 3, 1)
 
-        self.coordButton.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        grid.addWidget(self.coordButton, 1, 2, 2, 1)
+        grid.addWidget(self.freqLabel, 4, 0)
+        grid.addWidget(self.frequency, 4, 1)
 
-
-        grid.addWidget(self.okButton, 5, 0, 1, 2)
-        grid.addWidget(self.cancelButton, 5, 2)
-
+        grid.addWidget(self.okBtn, 5, 0, 1, 2)
+        grid.addWidget(self.cancelBtn, 5, 2)
 
         self.setLayout(grid)
 
+        # ------------ window properties ------------ #
         self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle("Auto Clicker")
+        self.setWindowTitle('Auto Clicker')
 
-        self.durationEdit.update()
-        self.frequencyEdit.update()
-        
+        # ------------ button connection ------------ #
+        self.coordBtn.clicked.connect(self.coordScreen)
+        self.okBtn.clicked.connect(partial(self.autoclick, self.duration.value(), self.frequency.value(), self.x.value(), self.y.value()))
 
-        # ---------- button connection ------------------------------
-        
-        self.coordButton.clicked.connect(self.coordScreen)
-        self.okButton.clicked.connect(partial(self.autoclick, self.durationEdit.value(), self.frequencyEdit.value(), self.xEdit.value(), self.yEdit.value()))
-        # self.okButton.clicked.connect(partial(self.autoclick, 1, 30, self.xEdit.value(), self.yEdit.value()))
 
-        # ---------- misc ------------------------------
-        
-        # self.setWindowIcon()
-        self.show()
-
-    # ---------- buttons ------------------------------
-
+    # ------------ button functions ------------ #
+    @QtCore.pyqtSlot()
     def coordScreen(self):
-        global w 
-        w = QCustomWidget()
-        w.show()
-        self.close()
+        self.coord = coordWindow()
+        self.coord.show()
+        self.x.setValue(self.coord.x)
+        
 
-    @staticmethod
-    def autoclick(dur, freq, x, y):
+    @QtCore.pyqtSlot()
+    def autoclick(self, dur, freq, x, y):
         print(f'{dur}, {freq}, {x}, {y}')
         for minutes in range(dur):
             for period in range(freq):
                 pyautogui.click(x, y, 1, 60/freq)
-            
-    # def closeEvent(self, event):
-    #     reply = QMessageBox.question(self, 'Auto Clicker',
-    #                                  'Are you sure you want to quit?', QMessageBox.Yes |
-    #                                  QMessageBox.No, QMessageBox.No)
 
-    #     if reply == QMessageBox.Yes:
-    #         event.accept()
-    #     else:
-    #         event.ignore()
-
-# ---------- coordinates window ------------------------------
-
-class QCustomLabel(QLabel):
+# ------------ Second Window Label ------------ #
+class coordLabel(QtWidgets.QLabel):
+     
     def __init__(self):
-        super().__init__()
+        super().__init__()  
         self.setMouseTracking(True)
         self.setTextLabelPosition(0, 0)
         self.setAlignment(QtCore.Qt.AlignCenter)
 
+
     def mouseMoveEvent(self, eventQMouseEvent):
         self.setTextLabelPosition(eventQMouseEvent.x(), eventQMouseEvent.y())
-        QWidget.mouseMoveEvent(self, eventQMouseEvent)
-
-    def mousePressEvent(self, eventQMouseEvent):
-        self.win = Window()
-        if eventQMouseEvent.button() == QtCore.Qt.LeftButton:
-            self.win.xEdit.setValue(self.x)
-            self.win.yEdit.setValue(self.y)
-            self.win.xEdit.update()
-            self.win.yEdit.update()
-            w.close()
-            
-        QWidget.mousePressEvent(self, eventQMouseEvent)
+        QtWidgets.QWidget.mouseMoveEvent(self, eventQMouseEvent)
 
     def setTextLabelPosition(self, x, y):
         self.x, self.y = x, y
-        self.setText(f'Please click on the screen *avoid the corners* ({self.x} : {self.y} )')
+        self.setText(f'Please click on the screen *avoid the corners* ({self.x}, {self.y})')
 
-
-class QCustomWidget(QWidget):
-    def __init__(self):
+# ------------ Second Window Overlay ------------ #
+class coordWindow(QtWidgets.QWidget):
+    def __init__(self, parent=mainWindow):
         super().__init__()
         self.setWindowOpacity(0.4)
-        # Init QLabel
-        self.positionQLabel = QCustomLabel()
-        # Init QLayout
-        layoutQHBoxLayout = QHBoxLayout()
-        layoutQHBoxLayout.addWidget(self.positionQLabel)
+        self.label = coordLabel()
+        layoutQHBoxLayout = QtWidgets.QHBoxLayout()
+        layoutQHBoxLayout.addWidget(self.label)    
         self.setLayout(layoutQHBoxLayout)
         self.showFullScreen()
+        self.x, self.y = 0, 0
+
+        self.w = parent
+
+    def mousePressEvent(self, eventQMouseEvent):
+        if eventQMouseEvent.button() == QtCore.Qt.LeftButton:
+            print(f'{self.label.x}, {self.label.y}')
+            self.x = self.label.x
+            self.y = self.label.y
+            print(f'{self.x}, {self.y}')
+
+            
+            self.close()
 
 
-#---------- main ------------------------------
+    
+class spinBox(QtWidgets.QSpinBox):
+
+    def __init__(self):
+        super().__init__()
+        
+        self.setMaximum(10000)
+        self.resize(self.sizeHint())
+
 
 def main():
-
-    app = QApplication(sys.argv)
-    window = Window()
-    sys.exit(app.exec())
+    app = QtWidgets.QApplication(sys.argv)
+    window = mainWindow()
+    window.show()
+    app.exec()
 
 if __name__ == '__main__':
     main()
+
